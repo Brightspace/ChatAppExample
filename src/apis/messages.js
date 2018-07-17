bbbc35b3fb327d621508f6fbccfd94516962115e
Dylan Pritchard <Dylan.Pritchard@D2L.com>
2018-07-17 12:21:45 -0400

    Get Messages

---
+++ src/apis/messages.js
@@ -0,0 +1,39 @@
+(function(){
+
+'use strict';
+
+const configs = require('../configurations'),
+      messageQueue = require('../messageQueue'),
+      request = require('superagent'),
+      express = require('express'),
+      router = express.Router();
+
+const createUrl = function(apiRoute) {
+    return configs.instanceScheme + '//' + configs.instanceUrl + ':' + configs.instancePort + apiRoute;
+};
+
+module.exports = function() {
+
+    var messages = [];
+    
+    /* GET /messages
+    *  Returns the list of messages
+    */
+    router.get('/messages', function (req, res) {
+
+        const accessToken = req.cookies[configs.cookieName].accessToken;
+        if (!accessToken) {
+            res.redirect('/');
+            return;
+        }
+
+        res.set('Content-Type', 'application/json');
+        res.status(200)
+            .end(JSON.stringify(messageQueue.messages));
+
+        return;
+    });
+
+    return router;
+};
+}());
--- src/chatsocket.js
+++ src/chatsocket.js
@@ -1,19 +1,44 @@
 (function(){
 'use strict';
 
-const socketIO = require('socket.io');
+const socketIO = require('socket.io'),
+      messagesQueue = require('./messageQueue');
+
+var onlineUsers = [];
 
 module.exports.init = function (httpsServer) {
-
-    var messages = [];
 
     var io = socketIO(httpsServer);
     io.on('connection', function (socket) {
 
+        onlineUsers.forEach(function (user, index) {
+            socket.emit('user online', user);
+        });
+
+        socket.on('user online', function (userId) {
+            console.log('User Online: ' + userId);
+            onlineUsers.push(userId);
+            socket.userId = userId;
+            io.emit('user online', userId);
+        });
+
+        socket.on('disconnect', function () {
+            if (socket.userId) {
+                console.log('User Offline: ' + socket.userId);
+                for (var i = 0; i < onlineUsers.length; i++) {
+                    if (onlineUsers[i] == socket.userId) {
+                        onlineUsers.splice(i, 1);
+                        break;
+                    }
+                }
+                io.emit('user offline', socket.userId);
+                socket.userId = undefined;
+            }
+        });
+
         socket.on('new message', function (msg) {
-            console.log('<inbound Message> ' + msg);
-            messages.push(msg);
-            io.emit('new message', msg + ' <transmitted from socket>');
+            messagesQueue.messages.push(msg);
+            io.emit('new message', msg);
         });
     });
 };
