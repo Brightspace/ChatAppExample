983e9491c5f9a365a7d819f519ef370bdc07d74e
Owen Smith <owen@omsmith.ca>
2019-06-18 10:11:28 -0400

    remove NODE_TLS_REJECT_UNAUTHORIZED (#16)
    This is a client override, so unless the node app is making calls to itself, it doesn't need to ignore certificate validation

--- src/server-local.js
+++ src/server-local.js
@@ -7,7 +7,6 @@ const https = require('https'),
       app = require('./server');
 
 const httpsPort = process.env.HTTPS_PORT || 34343;
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'; // Required for self signing cert.
 const options = {
     key: selfSigned.key,
     cert: selfSigned.cert
