1fabc3cd519b7a76a984610c1134eb6e1842b885
Owen Smith <owen@omsmith.ca>
2019-06-18 10:15:05 -0400

    Omsmith/chatsocket refact (#12)
    * use Set in chatsocket
    
    * lift callback scope

--- src/chatsocket.js
+++ src/chatsocket.js
@@ -4,42 +4,45 @@
 const socketIO = require('socket.io'),
       messagesQueue = require('./messageQueue');
 
-var onlineUsers = [];
+const onlineUsers = new Set();
 
 module.exports.init = function (httpsServer) {
 
     var io = socketIO(httpsServer);
     io.on('connection', function (socket) {
-
-        onlineUsers.forEach(function (user, index) {
+        onlineUsers.forEach(function (user) {
             socket.emit('user online', user);
         });
 
-        socket.on('user online', function (userId) {
-            console.log('User Online: ' + userId);
-            onlineUsers.push(userId);
-            socket.userId = userId;
-            io.emit('user online', userId);
-        });
-
-        socket.on('disconnect', function () {
-            if (socket.userId) {
-                console.log('User Offline: ' + socket.userId);
-                for (var i = 0; i < onlineUsers.length; i++) {
-                    if (onlineUsers[i] == socket.userId) {
-                        onlineUsers.splice(i, 1);
-                        break;
-                    }
-                }
-                io.emit('user offline', socket.userId);
-                socket.userId = undefined;
-            }
-        });
-
-        socket.on('new message', function (msg) {
-            messagesQueue.messages.push(msg);
-            io.emit('new message', msg);
-        });
+        socket.on('user online', onUserOnline);
+        socket.on('disconnect', onDisconnect);
+        socket.on('new message', onNewMessage);
     });
+
+    function onUserOnline(userId) {
+        const socket = this;
+
+        console.log('User Online: ' + userId);
+        onlineUsers.add(userId);
+        socket.userId = userId;
+        io.emit('user online', userId);
+    }
+
+    function onDisconnect() {
+        const socket = this;
+
+        if (socket.userId) {
+            console.log('User Offline: ' + socket.userId);
+            onlineUsers.delete(socket.userId);
+            io.emit('user offline', socket.userId);
+            socket.userId = undefined;
+        }
+    }
+
+    function onNewMessage(msg) {
+        messagesQueue.messages.push(msg);
+        io.emit('new message', msg);
+    }
+
 };
 }());
